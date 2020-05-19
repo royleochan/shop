8703eb84dac855df81e0b16949deb1e880644d6d
royleochan <royleochan@gmail.com>
2020-05-19 21:03:25 +0800

    adding show details logic

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -17,13 +17,18 @@ const CartItem = (props) => {
       </Text>
       <View style={styles.itemData}>
         <Text style={styles.title}>${props.amount}</Text>
-        <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
-          <Ionicons
-            name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
-            size={23}
-            color="red"
-          />
-        </TouchableOpacity>
+        {props.deletable && (
+          <TouchableOpacity
+            onPress={props.onRemove}
+            style={styles.deleteButton}
+          >
+            <Ionicons
+              name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+              size={23}
+              color="red"
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
@@ -32,24 +37,24 @@ const CartItem = (props) => {
 export default CartItem;
 
 const styles = StyleSheet.create({
-    cartItem: {
-        padding: 10,
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginHorizontal: 20,
-    },
-    itemData: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    quantity: {
-        fontFamily: 'open-sans',
-        color: '#888',
-        fontSize: 16
-    },
-    title: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 16
-    }
+  cartItem: {
+    padding: 10,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+  itemData: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  quantity: {
+    fontFamily: "open-sans",
+    color: "#888",
+    fontSize: 16,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16,
+  },
 });
--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-import CartItem from "./CartItem";
+import CartItem from './CartItem';
 import Colors from "../../constants/Colors";
 
 const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button color={Colors.primary} title="Show Details" />
+      <Button
+        color={Colors.primary}
+        title="Show Details"
+        onPress={() => {
+          setShowDetails((prevState) => !prevState);
+        }}
+      />
+      {showDetails && (
+        <View>
+          {props.items.map((item) => (
+            <CartItem
+              quantity={item.quantity}
+              amount={item.sum}
+              title={item.productTitle}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -19,31 +37,31 @@ const OrderItem = (props) => {
 export default OrderItem;
 
 const styles = StyleSheet.create({
-    orderItem: {
-        shadowColor: "black",
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: "white",
-        margin:20,
-        padding: 10
-    },
-    summary: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-        marginBottom: 15
-    },
-    totalAmount: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 16
-    },
-    date: {
-        fontFamily: 'open-sans',
-        fontSize: 16,
-        color: '#888'
-    }
+  orderItem: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    margin: 20,
+    padding: 10,
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 15,
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16,
+  },
+  date: {
+    fontFamily: "open-sans",
+    fontSize: 16,
+    color: "#888",
+  },
 });
