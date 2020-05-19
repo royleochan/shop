5f27bca5373200cfab0901fc46a6e40a610c5cfc
royleochan <royleochan@gmail.com>
2020-05-19 19:07:43 +0800

    Style order items  and using Moment.js

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,13 +4,19 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { useSelector } from "react-redux";
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
