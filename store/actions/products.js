b73ec12746b9458c05384edd856eaadcb032bd3b
royleochan <royleochan@gmail.com>
2020-05-20 15:15:05 +0800

    store products on firebase

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,14 +7,34 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price,
-    },
+  return async (dispatch) => {
+    // any async code can be inserted here
+    const response = await fetch("https://rn-shop-app-494cc.firebaseio.com/products.json", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    });
+
+    const resData = await response.json();
+
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price,
+      },
+    });
   };
 };
 
